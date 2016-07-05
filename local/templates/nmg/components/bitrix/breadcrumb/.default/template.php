baf6ee8bcfa5bdf1ede6287ebd1da25cbae88680
Sergey <st@webgk.ru>
2016-07-05 18:04:15 +0300

    SUP-4656: edit conditions

--- local/templates/nmg/components/bitrix/breadcrumb/.default/template.php
+++ local/templates/nmg/components/bitrix/breadcrumb/.default/template.php
@@ -17,12 +17,20 @@ $strReturn = '<div class="crumbs">';
 
 for($index = 0, $itemSize = count($arResult); $index < $itemSize; $index++)
 {
-    if($index > 0)
+    if($index > 0) {
         $strReturn .= ' &rarr; ';
+    }
     $title = htmlspecialcharsex($arResult[$index]["TITLE"]);
-    if($arResult[$index]["LINK"] <> "" && $index+1 < count($arResult))
-        $strReturn .= '<span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a'.((strpos($APPLICATION->GetCurDir(), "/basket/order/")===0 && (strpos($arResult[$index]["LINK"], "/basket/order/"))===0) || $title == "Способ оплаты"?' class="backstep"':'').' href="'.$arResult[$index]["LINK"].'" title="'.$title.'"><span itemprop="title">'.$title.'</span></a></span>';
-    else $strReturn .= utf8win1251($title);
+    if($arResult[$index]["LINK"] <> "" && $index + 1 < count($arResult)) {
+        $class_string = (strpos($APPLICATION->GetCurDir(), "/basket/order/") == 0 && (strpos($arResult[$index]["LINK"], "/basket/order/")) == 0) || $title == "Способ оплаты"?' class="backstep"' : '';
+        $strReturn .= '<span itemscope itemtype="http://data-vocabulary.org/Breadcrumb">
+            <a' . $class_string . ' href="' . $arResult[$index]["LINK"] . '" title="' . $title . '">
+                <span itemprop="title">' . $title . '</span>
+            </a>
+        </span>';
+    } else {
+        $strReturn .= utf8win1251($title);
+    }
 }
 $strReturn .= '</div>';
 
